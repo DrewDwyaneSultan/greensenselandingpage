4b1da64d0cb0fc0c3403551adc363baa279bd0d9
Drew Dwyane B. Sultan <dwyanelycann@gmail.com>
2026-01-21 14:35:43 +0800

    Create script.js

---
+++ script.js
@@ -0,0 +1,10 @@
+// mobile menu toggling !!
+document.querySelector('.menu-toggle').addEventListener('click', function() {
+    document.querySelector('.nav-links').classList.toggle('active');
+});
+// for the tirple dashes in header :))
+document.querySelectorAll('.nav-links a').forEach(link => {
+    link.addEventListener('click', () => {
+        document.querySelector('.nav-links').classList.remove('active');
+    });
+});
